b78ab23d2c764a5cdb9361c0bd94b68b7d8c48d3
ryotokuro <ryotokuro@gmail.com>
2019-08-11 14:27:23 +1000

    added javascript empty so I can experiment with react

---
+++ blog/GitHub.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class GitHub extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#github_container');
+ReactDOM.render(e(GitHub), domContainer);
